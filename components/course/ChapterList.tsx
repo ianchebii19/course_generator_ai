2b9a0bf13aa8f6b2db956bc376a80d1bfd2982c5
Deborah Chebii <chebiideborah@gmail.com>
2025-02-19 07:58:15 +0300

    chapterlist

--- components/course/ChapterList.tsx
+++ components/course/ChapterList.tsx
@@ -2,7 +2,6 @@ import EditChapter from './EditChapter';
 import { Course } from '@/types'; // ✅ Import unified Course type
 
 interface ChapterListProps {
-  courseOutput: Course['courseOutput'];
   course: Course | null;
   edit?: boolean;
 }
